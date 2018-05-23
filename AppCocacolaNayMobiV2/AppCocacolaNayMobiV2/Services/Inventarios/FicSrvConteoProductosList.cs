2f2938ca5f10ad55d5abebd5b3b2b27984eb6610
brayanAP <brulariaspe@gmail.com>
2018-05-23 18:30:40 -0500

    brayanAP

---
+++ AppCocacolaNayMobiV2/AppCocacolaNayMobiV2/Services/Inventarios/FicSrvConteoProductosList.cs
@@ -0,0 +1,11 @@
+﻿using AppCocacolaNayMobiV2.Interfaces.Inventarios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCocacolaNayMobiV2.Services.Inventarios
+{
+    interface FicSrvConteoProductosList : IFicSrvConteoProductos
+    {
+    }
+}
